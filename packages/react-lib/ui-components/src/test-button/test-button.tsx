98e8ebcd2bd6c50d9ca9fbbb32e687cb32e32ba8
Rishikant Tiwari <rishikant.tiwari@hopscotch.in>
2020-07-07 12:46:26 +0530

    Added test button using Emotion

--- packages/react-lib/ui-components/src/test-button/test-button.tsx
+++ packages/react-lib/ui-components/src/test-button/test-button.tsx
@@ -1,7 +1,15 @@
+import React, { FC, ButtonHTMLAttributes } from 'react';
 import styled from '@emotion/styled';
 
-export const TestButton = styled.button`
-background:pink;
-width:40px;
-height:20px;
+export interface ITestButton extends ButtonHTMLAttributes < HTMLButtonElement > {
+    displayText?: String;
+}
+export const TestButtonStyled = styled.button`
+  background: pink;
+  width: 40px;
+  height: 20px;
 `;
+
+export const TestButton: FC<ITestButton> = (
+  props: ITestButton
+) => <TestButtonStyled {...props}>{props.displayText}</TestButtonStyled>;
