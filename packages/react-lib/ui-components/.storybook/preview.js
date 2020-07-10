37a25346efb9ce3601f71cf42e63554a1f26aa8f
Rishikant Tiwari <rishikant.tiwari@hopscotch.in>
2020-07-11 02:52:05 +0530

    Updated Storybook configs

--- packages/react-lib/ui-components/.storybook/preview.js
+++ packages/react-lib/ui-components/.storybook/preview.js
@@ -1,5 +1,11 @@
 import { configure, addParameters } from '@storybook/react';
+import '@storybook/addon-console';
+import { setConsoleOptions } from '@storybook/addon-console';
+import hsTheme from './hsTheme';
 addParameters({
+  options: {
+    theme: hsTheme,
+  },
   backgrounds: [
     { name: 'Hs-Content', value: '#f5f5f5', default: true },
     { name: 'HS-Dark', value: '#3e4855' },
@@ -7,3 +13,6 @@ addParameters({
   ],
 });
 configure(require.context('../src/', true, /\.stories\.(ts|tsx)$/), module);
+setConsoleOptions({
+  panelExclude: [],
+});
