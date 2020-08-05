da120a8f3a774a733a03b58c05d48760b2c442cc
Rishikant Tiwari <rishikant.tiwari@hopscotch.in>
2020-08-05 19:44:14 +0530

    Removed Dummy components

--- packages/react-lib/ui-components/src/index.tsx
+++ packages/react-lib/ui-components/src/index.tsx
@@ -1,7 +1,3 @@
-export * from './thing/thing';
-export * from './thing2/thing2';
-export * from './test-button/test-button';
-export * from './test-button/test-button-emotion-core';
 export * from './left-nav';
 export * from './button';
 export * from './image-upload';
