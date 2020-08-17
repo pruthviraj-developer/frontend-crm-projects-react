f7649352adfb1d29559597f3570936d2f5120417
Pruthviraj Patel <pruthviraj@hopscotch.in>
2020-08-17 11:30:11 +0530

    Date picker changes

---
+++ packages/react-lib/ui-components/src/date-picker/date-picker.tsx
@@ -0,0 +1,50 @@
+import React, { FC, useState } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import './date-picker.css';
+import styled from '@emotion/styled';
+import { format, isValid, subDays } from 'date-fns';
+
+interface Props {
+  placeholder: string;
+  dateFormat: string;
+  isEndDate?: string;
+  selectedDate?: string;
+  handleChange?: (event: React.MouseEvent) => void;
+  disabled?: true | false;
+  disableMinDate?: true | false;
+}
+
+const DateTimePicker = styled.div``;
+
+export const DatePickerComponent: FC<Props> = (props: Props) => {
+  const selectedDate = props.selectedDate
+    ? new Date(props.selectedDate)
+    : new Date();
+  const [startDate, setStartDate] = useState<any>(selectedDate);
+  const onDateChange = (date: Date | any) => {
+    setStartDate(date);
+    // console.log(format(date, "yyyy-MM-dd'T'hh:mm:ss"));
+  };
+  const handleOnBlur = ({ target: { value } }) => {
+    const date = new Date(value);
+    if (isValid(date)) {
+      onDateChange(date);
+    } else {
+      // console.log('value: ', value, date);
+    }
+  };
+  return (
+    <DateTimePicker>
+      <DatePicker
+        selected={startDate}
+        showTimeSelect
+        dateFormat={props.dateFormat}
+        onBlur={handleOnBlur}
+        onChange={(date) => date && onDateChange(date)}
+        minDate={subDays(new Date(), 0)}
+        placeholderText={props.placeholder}
+      />
+    </DateTimePicker>
+  );
+};
