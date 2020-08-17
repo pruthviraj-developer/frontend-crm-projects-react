b70613560ec62f9e9b952bdff094f6628048f61e
Pruthviraj Patel <pruthviraj@hopscotch.in>
2020-08-18 00:34:14 +0530

    Table design changes

--- packages/react-lib/ui-components/src/hs-table/HsTable.tsx
+++ packages/react-lib/ui-components/src/hs-table/HsTable.tsx
@@ -1,74 +1,129 @@
 import React, { FC } from 'react';
+import { forwardRef } from 'react';
 import MaterialTable from 'material-table';
+import { format } from 'date-fns';
+import {
+  ChevronRight,
+  ChevronLeft,
+  FirstPage,
+  LastPage,
+  ArrowDownward,
+} from '@material-ui/icons';
+
+import DeleteForeverTwoToneIcon from '@material-ui/icons/DeleteForeverTwoTone';
+import FileCopyTwoToneIcon from '@material-ui/icons/FileCopyTwoTone';
+import PublishTwoToneIcon from '@material-ui/icons/PublishTwoTone';
 
 interface TableProps {
   title?: string;
 }
+
 export const HSTable: FC<TableProps> = (props: TableProps) => {
   const [state, setState] = React.useState({
     columns: [
-      { title: 'Name', field: 'name' },
-      { title: 'Surname', field: 'surname' },
-      { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
+      { title: 'ID', field: 'id' },
+      { title: 'Title', field: 'title' },
       {
-        title: 'Birth Place',
-        field: 'birthCity',
-        lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
+        title: 'Sorted By',
+        render: (rowData) => {
+          if (rowData && rowData.sorts && rowData.sorts.length) {
+            return rowData.sorts.join(',');
+          }
+          return '';
+        },
+      },
+      { title: 'Created By', field: 'createdBy' },
+      { title: 'Updated By', field: 'updatedBy' },
+      {
+        title: 'Created On',
+        render: (rowData) => {
+          if (rowData && rowData.createdOn) {
+            return format(new Date(rowData.createdOn), 'dd-MM-yy');
+          }
+          return 'NA';
+        },
+      },
+      {
+        title: 'Last updated',
+        render: (rowData) => {
+          if (rowData && rowData.updatedOn) {
+            return format(new Date(rowData.updatedOn), 'dd-MM-yy');
+          }
+          return 'NA';
+        },
       },
     ],
     data: [
-      { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
       {
-        name: 'Zerya Betül',
-        surname: 'Baran',
-        birthYear: 2017,
-        birthCity: 34,
+        id: 1,
+        title: 'Non Hero Carousel',
+        birthYear: 1987,
+        birthCity: 63,
+        createdBy: 'info@nstechs.com',
+        updatedBy: 'ayesha.siddiqa@hopscotch.in',
+        updatedOn: '2020-08-17T17:15:17',
+        createdOn: '2020-08-11T15:44:07',
+        sorts: [
+          'Boy 0-1 years',
+          'Boy 1-6 years',
+          'Boy 6+ years',
+          'Girl 0-1 years',
+          'Girl 1-6 years',
+          'Girl 6+ years',
+          'Sale',
+          'All',
+        ],
       },
     ],
   });
 
+  const tableIcons = {
+    DeleteForeverIcon: forwardRef((props, ref) => (
+      <DeleteForeverTwoToneIcon {...props} ref={ref} />
+    )),
+    PublishIcon: forwardRef((props, ref) => (
+      <PublishTwoToneIcon {...props} ref={ref} />
+    )),
+    Clone: forwardRef((props, ref) => (
+      <FileCopyTwoToneIcon {...props} ref={ref} />
+    )),
+    FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+    LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+    NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+    PreviousPage: forwardRef((props, ref) => (
+      <ChevronLeft {...props} ref={ref} />
+    )),
+    SortArrow: forwardRef((props, ref) => (
+      <ArrowDownward {...props} ref={ref} />
+    )),
+  };
   return (
     <MaterialTable
+      options={{
+        actionsColumnIndex: -1,
+        search: false,
+      }}
+      actions={[
+        {
+          icon: tableIcons.PublishIcon,
+          tooltip: 'Publish',
+          onClick: (event) => alert('You want to add a new row'),
+        },
+        {
+          icon: tableIcons.Clone,
+          tooltip: 'Clone',
+          onClick: (event) => alert('You want to add a new row'),
+        },
+        {
+          icon: tableIcons.DeleteForeverIcon,
+          tooltip: 'Delete',
+          onClick: (event) => alert('You want to add a new row'),
+        },
+      ]}
+      icons={tableIcons}
       title={props.title}
       columns={state.columns}
       data={state.data}
-      editable={{
-        onRowAdd: (newData) =>
-          new Promise((resolve) => {
-            setTimeout(() => {
-              resolve();
-              setState((prevState) => {
-                const data = [...prevState.data];
-                data.push(newData);
-                return { ...prevState, data };
-              });
-            }, 600);
-          }),
-        onRowUpdate: (newData, oldData) =>
-          new Promise((resolve) => {
-            setTimeout(() => {
-              resolve();
-              if (oldData) {
-                setState((prevState) => {
-                  const data = [...prevState.data];
-                  data[data.indexOf(oldData)] = newData;
-                  return { ...prevState, data };
-                });
-              }
-            }, 600);
-          }),
-        onRowDelete: (oldData) =>
-          new Promise((resolve) => {
-            setTimeout(() => {
-              resolve();
-              setState((prevState) => {
-                const data = [...prevState.data];
-                data.splice(data.indexOf(oldData), 1);
-                return { ...prevState, data };
-              });
-            }, 600);
-          }),
-      }}
     />
   );
 };
