58b3d7168085969ea8b57dcc6b5bdb1472ab7d85
Pruthviraj Patel <pruthviraj@hopscotch.in>
2020-08-20 23:03:06 +0530

    Carousel page form validation etc

--- web-apps/react-carousel-crm/src/components/creator/CarouselCreator.tsx
+++ web-apps/react-carousel-crm/src/components/creator/CarouselCreator.tsx
@@ -19,63 +19,51 @@ interface Values {
   start_date: string;
   end_date: string;
 }
+
 const CarouselCreator: FC = () => {
   const platform = [
     { key: 'IOS', name: 'IOS' },
     { key: 'ANDROID', name: 'ANDROID' },
   ];
-  const ranges = [
-    {
-      value: 'none',
-      label: 'None',
-    },
-    {
-      value: '0-20',
-      label: '0 to 20',
-    },
-    {
-      value: '21-50',
-      label: '21 to 50',
-    },
-    {
-      value: '51-100',
-      label: '51 to 100',
-    },
-  ];
   return (
-    <MuiPickersUtilsProvider utils={DateFnsUtils}>
-      <Formik
-        initialValues={{
-          title: '',
-          type: '',
-          sort: [],
-          position: '',
-          platform: [],
-          start_date: undefined,
-          end_date: undefined,
-        }}
-        validate={(values) => {
-          const errors: Partial<Values> = {};
-          if (!values.title) {
-            errors.title = 'Title is required';
-          }
-          if (!values.type) {
-            errors.type = 'Carousel type is required';
-          }
-          if (!values.position) {
-            errors.position = 'Position is required';
-          }
+    <Formik
+      initialValues={{
+        title: '',
+        type: [],
+        sort: [],
+        position: '',
+        platform: [],
+        start_date: undefined,
+        end_date: undefined,
+      }}
+      validate={(values) => {
+        const errors: Partial<Values> = {};
+        if (!values.title) {
+          errors.title = 'Title is required';
+        }
+        if (!values.position) {
+          errors.position = 'Position is required';
+        }
+        if (!(values.type && values.type.length)) {
+          errors.type = 'Carousel type is required';
+        }
+        if (!(values.platform && values.platform.length)) {
+          errors.platform = 'Platform is required';
+        }
+        if (!(values.sort && values.sort.length)) {
+          errors.sort = 'Sort is required';
+        }
 
-          return errors;
-        }}
-        onSubmit={(values, { setSubmitting }) => {
-          setTimeout(() => {
-            setSubmitting(false);
-            alert(JSON.stringify(values, null, 2));
-          }, 500);
-        }}
-      >
-        {({ submitForm, isSubmitting }) => (
+        return errors;
+      }}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          setSubmitting(false);
+          alert(JSON.stringify(values, null, 2));
+        }, 500);
+      }}
+      render={({ submitForm, isSubmitting, touched, errors }) => (
+        <MuiPickersUtilsProvider utils={DateFnsUtils}>
           <Form>
             <Box margin={1}>
               <Field component={TextField} style={{ width: 350 }} name="title" type="text" label="Carousel Title" />
@@ -85,11 +73,12 @@ const CarouselCreator: FC = () => {
             </Box>
             <Box margin={1}>
               <Field
+                required
                 name="type"
                 variant="standard"
                 component={Autocomplete}
-                options={ranges}
-                getOptionLabel={(option: any) => option.label}
+                options={platform}
+                getOptionLabel={(option: any) => (option.name ? option.name : option)}
                 style={{ width: 350 }}
                 renderInput={(params: AutocompleteRenderInputParams) => (
                   <MuiTextField {...params} label="Carousel Type" variant="outlined" />
@@ -104,10 +93,16 @@ const CarouselCreator: FC = () => {
                 variant="standard"
                 component={Autocomplete}
                 options={platform}
-                getOptionLabel={(option: any) => option.name}
+                getOptionLabel={(option: any) => (option.name ? option.name : option)}
                 style={{ width: 350 }}
                 renderInput={(params: AutocompleteRenderInputParams) => (
-                  <MuiTextField {...params} label="Platform" variant="outlined" />
+                  <MuiTextField
+                    {...params}
+                    error={touched['platform'] && !!errors['platform']}
+                    helperText={touched['platform'] && errors['platform']}
+                    label="Platform"
+                    variant="outlined"
+                  />
                 )}
               />
             </Box>
@@ -120,28 +115,33 @@ const CarouselCreator: FC = () => {
                 helperText="Please select sort"
                 component={Autocomplete}
                 options={platform}
-                getOptionLabel={(option: any) => option.name}
+                getOptionLabel={(option: any) => (option.name ? option.name : option)}
                 style={{ width: 350 }}
                 renderInput={(params: AutocompleteRenderInputParams) => (
-                  <MuiTextField {...params} label="Sort" variant="outlined" />
+                  <MuiTextField
+                    {...params}
+                    error={touched['sort'] && !!errors['sort']}
+                    helperText={touched['sort'] && errors['sort']}
+                    label="Sort"
+                    variant="outlined"
+                  />
                 )}
               />
             </Box>
+            {isSubmitting && <LinearProgress />}
             <Box margin={1}>
               <Field component={DateTimePicker} style={{ width: 350 }} name="start_date" label="Start Date" />
             </Box>
             <Box margin={1}>
               <Field component={DateTimePicker} style={{ width: 350 }} name="end_date" label="End Date" />
             </Box>
-            {isSubmitting && <LinearProgress />}
-            <br />
             <Button variant="contained" color="primary" disabled={isSubmitting} onClick={submitForm}>
               Submit
             </Button>
           </Form>
-        )}
-      </Formik>
-    </MuiPickersUtilsProvider>
+        </MuiPickersUtilsProvider>
+      )}
+    />
   );
 };
 export default CarouselCreator;
