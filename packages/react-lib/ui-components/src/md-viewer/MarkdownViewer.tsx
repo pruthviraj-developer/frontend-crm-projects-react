cd2e1e7a170416652c46b4c5720275e5b95c2f2a
Rishikant Tiwari <rishikant.tiwari@hopscotch.in>
2020-09-02 19:28:45 +0530

    Styled Table in MarkDown

--- packages/react-lib/ui-components/src/md-viewer/MarkdownViewer.tsx
+++ packages/react-lib/ui-components/src/md-viewer/MarkdownViewer.tsx
@@ -2,16 +2,33 @@ import React, { useEffect, useState } from 'react';
 import ReactMarkdown from 'react-markdown/with-html';
 import styled from '@emotion/styled';
 import { MarkdownViewerProps } from './IMarkdownViewer';
-
+import { Colors } from '@hs/utils';
 const StyledMDViewer = styled.div`
   & > h1 {
     text-align: center;
   }
-  & > h2 {
+  & > h2,
+  h3,
+  h4,
+  h5,
+  h6 {
     text-align: left;
   }
   text-align: left;
   margin-left: 90px;
+  table {
+    border: 1px solid black;
+    border-collapse: collapse;
+  }
+  th,
+  td {
+    border: 1px solid black;
+    text-align: left;
+    padding: 0.8rem;
+  }
+  tr:nth-of-type(even) {
+    background-color: ${Colors.GREY_SHADE[400]};
+  }
 `;
 export const MarkdownViewer = ({ docUrl }: MarkdownViewerProps) => {
   const [fileText, setFileText] = useState('');
