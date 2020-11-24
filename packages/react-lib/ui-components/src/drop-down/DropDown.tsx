f27e41303f42d0e2b03d27845085bb7ad88763c9
Rishikant Tiwari <rishikant.tiwari@hopscotch.in>
2020-11-24 23:55:44 +0530

    Fixed Yarn package issue happend for the ncarousel change

--- packages/react-lib/ui-components/src/drop-down/DropDown.tsx
+++ packages/react-lib/ui-components/src/drop-down/DropDown.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react';
 import styled from '@emotion/styled';
-import { css } from '@emotion/core';
+import css from '@emotion/css';
 import { DropDownProps } from './IDropDown';
 import { useState, useRef, useEffect } from 'react';
 import { Colors } from '@hs/utils';
