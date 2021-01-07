e69179f546ae5d2bf0b1afb446ce2e5e08987838
Rishikant Tiwari <rishikant.tiwari@hopscotch.in>
2021-01-07 15:14:35 +0530

    Created reorder project for checks and balances

---
+++ web-apps/react-reorder-crm/src/components/DashBoard.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+interface Props {}
+
+export const DashBoard = (props: Props) => {
+  return (
+    <div>
+      <h1>Checks and Balances DashBoard</h1>
+    </div>
+  );
+};
