486ebd1b2a2fc58cd6dad6132954c8bed12ae196
Pruthviraj Patel <pruthviraj@hopscotch.in>
2021-01-08 11:29:40 +0530

    WIP filters

---
+++ packages/react-lib/ui-components/src/hs-filters/HSfilters.stories.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+import { HsFilters } from './HSfilters';
+export default {
+  title: 'HsFilters',
+  component: HsFilters,
+};
+
+export const HsFiltersComponent: FC = () => (
+    <HsFilters  />
+  );
+  
