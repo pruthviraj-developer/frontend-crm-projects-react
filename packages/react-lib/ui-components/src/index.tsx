1d019bbd628d253135aba00e8c84328ed981a31c
Pruthviraj Patel <pruthviraj@hopscotch.in>
2021-01-13 14:14:57 +0530

    Exporting the filters and chips

--- packages/react-lib/ui-components/src/index.tsx
+++ packages/react-lib/ui-components/src/index.tsx
@@ -12,3 +12,6 @@ export * from './hs-table-v2';
 export * from './snack-bar';
 export * from './md-viewer';
 export * from './file-upload';
+export * from './hs-selectable-table';
+export * from './hs-filters';
+export * from './hs-chips';
