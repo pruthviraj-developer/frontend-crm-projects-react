2cbd99ff726edba41b6af306c769e687af11be6e
Pruthviraj Patel <pruthviraj@hopscotch.in>
2021-01-13 13:27:06 +0530

    Using useEffect to set the updated variables

--- packages/react-lib/ui-components/src/filter-list-page/FilterListPage.stories.tsx
+++ packages/react-lib/ui-components/src/filter-list-page/FilterListPage.stories.tsx
@@ -3,48 +3,48 @@ import { FilterListPage } from './FilterListPage';
 import { FiltersOptions } from './IFilterListPage';
 export default {
   title: 'HsFilerListPage',
-  component: FilterListPage
+  component: FilterListPage,
 };
 
 const reasonOptions = [
-    {
-      display: 'Non due to quality and sizing',
-      value: '1kjh',
-      key: '1kjh',
-      id: '1',
-    },
-    {
-      display: 'Proc high return due to other reason',
-      value: 'lkj2',
-      key: 'lkj2',
-      id: '2',
-    },
-  ];
-  
-  const reasonSideBarOption: FiltersOptions = {
-    isSelect: true,
-    name: 'sreason',
-    label: 'mReason',
-    options: reasonOptions,
-    type: 'autocomplete',
-  };
-  
-  const testFieldSideBarOption: FiltersOptions = {
-    isSelect: true,
-    name: 'treason',
-    label: 'Text Field Reason',
-  };
-  
-  const autoSideBarOption: FiltersOptions = {
-    isSelect: true,
-    name: 'areason',
-    label: 'Auto Reason',
-    options: reasonOptions,
-    type: 'autocomplete',
-  };
+  {
+    display: 'Non due to quality and sizing',
+    value: '1kjh',
+    key: '1kjh',
+    id: '1',
+  },
+  {
+    display: 'Proc high return due to other reason',
+    value: 'lkj2',
+    key: 'lkj2',
+    id: '2',
+  },
+];
 
-const data = {
-    sideBar:[ reasonSideBarOption, testFieldSideBarOption, autoSideBarOption]
+const reasonSideBarOption: FiltersOptions = {
+  isSelect: true,
+  name: 'sreason',
+  label: 'mReason',
+  options: reasonOptions,
+  type: 'autocomplete',
 };
 
-export const FilerListPageComponent: FC = () => <FilterListPage {...data} />;
+const testFieldSideBarOption: FiltersOptions = {
+  isSelect: true,
+  name: 'treason',
+  label: 'Text Field Reason',
+};
+
+const autoSideBarOption: FiltersOptions = {
+  isSelect: true,
+  name: 'areason',
+  label: 'Auto Reason',
+  options: reasonOptions,
+  type: 'autocomplete',
+};
+
+const data = {
+  sideBar: [reasonSideBarOption, testFieldSideBarOption, autoSideBarOption],
+};
+
+export const FilterListPageComponent: FC = () => <FilterListPage {...data} />;
--- packages/react-lib/ui-components/src/hs-chips/HsChips.tsx
+++ packages/react-lib/ui-components/src/hs-chips/HsChips.tsx
@@ -1,57 +1,47 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useState, useEffect } from 'react';
 import Chip from '@material-ui/core/Chip';
 import { makeStyles } from '@material-ui/core/styles';
 import { IHsChips, Ilist, FiltersOption } from './IHsChips';
 
 const useStyles = makeStyles({
-  chips : {
-    margin:'5px'
-  }
+  chips: {
+    margin: '5px',
+  },
 });
 
-// export const HsChips: FC<IHsChips> = ({objectsList}: IHsChips) => {
-//   const classes = useStyles();
-//   console.log('Obj List', objectsList);
-//   const [data, setData] = useState(objectsList || []);
-//   const handleDelete = (arrayindex:number, index:number) => {
-//     const obj  = {...data[arrayindex]};
-//     obj.options.splice(index,1);
-//     data.splice(arrayindex,1);
-//     setData([ ...data,obj]);
-//   };
-
-//   return (
-//     <div>
-//       {/* {JSON.stringify(objectsList)} */}
-//         {data.map((obj:Ilist, arrayindex) => (
-//             obj.options.map(
-//               (listObject:FiltersOption,index:number) => 
-//               (<Chip key={listObject.key || index} label={listObject.display} onDelete={() => {handleDelete(arrayindex, index);}} color="primary"  className={classes.chips}/>))
-//         ))}
-//     </div>
-//   );
-// };
-
-export const HsChips: FC<IHsChips> = ({objectsList,updateRemovedFilters}: IHsChips) => {
-  let list = [...objectsList];
+export const HsChips: FC<IHsChips> = ({
+  objectsList,
+  updateRemovedFilters,
+}: IHsChips) => {
   const classes = useStyles();
-  const [data, setData] = useState(list || []);
-  const handleDelete = (arrayindex:number, index:number) => {
-    const obj  = {...list[arrayindex]};
-    obj.options.splice(index,1);
-    list.splice(arrayindex,1);
-    list = [ ...list,obj];
-    setData(list);
-    updateRemovedFilters && updateRemovedFilters(list);
+  const [data, setData] = useState(objectsList || []);
+  useEffect(() => {
+    setData(objectsList);
+  }, [objectsList]);
+  const handleDelete = (arrayindex: number, index: number) => {
+    const obj = { ...data[arrayindex] };
+    obj.options.splice(index, 1);
+    data.splice(arrayindex, 1);
+    const updatedDataObject = [...data, obj];
+    setData(updatedDataObject);
+    updateRemovedFilters && updateRemovedFilters(updatedDataObject);
   };
 
   return (
     <div>
-        {list.map((obj:Ilist, arrayindex) => (
-            obj.options.map(
-              (listObject:FiltersOption,index:number) => 
-              (<Chip key={listObject.key || index} label={listObject.display} onDelete={() => {handleDelete(arrayindex, index);}} color="primary"  className={classes.chips}/>))
-        ))}
+      {data.map((obj: Ilist, arrayindex) =>
+        obj.options.map((listObject: FiltersOption, index: number) => (
+          <Chip
+            key={listObject.key || index}
+            label={listObject.display}
+            onDelete={() => {
+              handleDelete(arrayindex, index);
+            }}
+            color="primary"
+            className={classes.chips}
+          />
+        ))
+      )}
     </div>
   );
 };
