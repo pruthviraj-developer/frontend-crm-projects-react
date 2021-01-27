e0a02fae1160052a1f0c7f9079543121f33a4cd4
Pruthviraj Patel <pruthviraj@hopscotch.in>
2021-01-27 20:32:53 +0530

    Filters api call only once

--- web-apps/react-reorder-crm/src/components/DashBoard.tsx
+++ web-apps/react-reorder-crm/src/components/DashBoard.tsx
@@ -219,7 +219,7 @@ export const DashBoard = () => {
         setFiltersMessage('Filters not available try later');
       }
     })();
-  }, [filtersList]);
+  }, []);
 
   useEffect(() => {
     (async () => {
