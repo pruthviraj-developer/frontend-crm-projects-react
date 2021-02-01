b9ec374452ac860940b9db15ea0b84387f20d56a
Pruthviraj Patel <pruthviraj@hopscotch.in>
2021-02-01 18:38:45 +0530

    Messages added

--- web-apps/react-reorder-crm/src/components/DashBoard.tsx
+++ web-apps/react-reorder-crm/src/components/DashBoard.tsx
@@ -207,14 +207,14 @@ export const DashBoard = () => {
     showConfirmDialog(true);
     setActionRows(postObject);
     const total = postObject.skus.length;
-    setConfirmationMessage(`Approve quantity for ${total} ${total > 1 ? 'SKUs?' : 'SKU'}`);
+    setConfirmationMessage(`Cancel quantity for ${total} ${total > 1 ? 'SKUs?' : 'SKU'}`);
   };
 
   const exportColumn = (postObject: any) => {
     showConfirmDialog(true);
     setActionRows(postObject);
     const total = postObject.skus.length;
-    setConfirmationMessage(`Cancel quantity for ${total} ${total > 1 ? 'SKUs?' : 'SKU'}`);
+    setConfirmationMessage(`Approve quantity for ${total} ${total > 1 ? 'SKUs?' : 'SKU'}`);
   };
 
   const modifySelectedColumns = (postObject: any) => {
