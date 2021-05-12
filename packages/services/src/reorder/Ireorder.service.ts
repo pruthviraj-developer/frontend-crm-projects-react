742c94ba7d8feadb38919348eddfee3f9c0a8e9a
Pruthviraj Patel <pruthviraj@hopscotch.in>
2021-05-12 10:00:47 +0530

    Added new file

---
+++ packages/services/src/reorder/Ireorder.service.ts
@@ -0,0 +1,49 @@
+interface OptionType {
+  key: string | number;
+  value: string;
+  second: string | number;
+  first: string | number;
+}
+
+interface BrandListEntity {
+  id: number;
+  display: string;
+}
+
+export interface ICreateClusterType {
+    vendor_id: OptionType;
+    brand_id: BrandListEntity;
+    category_id: OptionType;
+    sub_category_id: OptionType;
+    product_type_id: OptionType;
+    gender: OptionType;
+    age_constraints?: AgeConstraintsEntity[] | null;
+    color_constraints?: ColorConstraintsEntity[] | null;
+    attribute: Attribute;
+  }
+  
+  interface AgeConstraintsEntity {
+    from: number;
+    to: number;
+  }
+  
+  interface ColorConstraintsEntity {
+    display: string;
+    key: string;
+  }
+
+  interface Attribute {
+    key: string;
+    name: string;
+    type: string;
+  }
+
+  export interface IReorderCreateConstraint {
+    action: string;
+    message: string;
+  }
+  
+  export interface IReorderCreateConstraintParams {
+    id: string,
+    group_id: string
+  }
