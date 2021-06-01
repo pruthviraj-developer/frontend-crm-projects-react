9cacf375cf90d68f4f920bd894f125e887ad652f
Rishikant Tiwari <rishikant.tiwari@hopscotch.in>
2021-06-01 10:17:19 +0530

    Added Use Category hook

--- packages/react-lib/hooks/src/use-category/IUseCategory.ts
+++ packages/react-lib/hooks/src/use-category/IUseCategory.ts
@@ -1,12 +1,12 @@
 export interface ISubCategory {
-  sub_cat?: OptionType[] | null;
+  sub_cat: OptionType[] | null;
 }
 
 export interface IProductTypes {
   pt?: OptionType[] | null;
 }
 
-interface OptionType {
+export interface OptionType {
   key: string | number;
   value: string;
   second: string | number;
@@ -14,6 +14,6 @@ interface OptionType {
 }
 
 export interface IUseCategoryProps {
-  category_id: string;
+  category_id?: string;
   sub_category_ids?: string[];
 }
