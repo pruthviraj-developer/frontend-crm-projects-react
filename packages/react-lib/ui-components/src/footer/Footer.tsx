a201199501b7ff7e13a6bfdd4b87488784286471
Pruthviraj Patel <pruthviraj@hopscotch.in>
2021-07-28 19:25:51 +0530

    Added images to the footer

--- packages/react-lib/ui-components/src/footer/Footer.tsx
+++ packages/react-lib/ui-components/src/footer/Footer.tsx
@@ -1,27 +1,70 @@
 import React, { FC } from 'react';
 import { IFooterProps } from './Ifooter';
+import { AppleIcon, PlayIcon, MailIcon, InfoIcon } from '@hs/icons';
 import {
   Copyright,
+  DownloadApps,
+  DownloadIcons,
+  DownloadIcon,
   FooterWrapper,
-  FooterElements,
-  FooterElement,
+  FooterSection,
+  FooterBlockTitle,
+  FooterLinks,
+  FooterLink,
   Link,
+  LinkWithIcons,
+  FooterBlock,
+  StyledIcon,
 } from './FooterCss';
 export const Footer: FC<IFooterProps> = ({}: IFooterProps) => {
   const currentYear = new Date().getFullYear();
   return (
     <FooterWrapper>
-      <FooterElements>
-        <FooterElement>
+      <FooterSection>
+        <FooterBlock>
+          <LinkWithIcons href="/helpcenter">
+            <StyledIcon icon={InfoIcon}></StyledIcon>
+            Need help?
+          </LinkWithIcons>
+        </FooterBlock>
+        <FooterBlock>
+          <LinkWithIcons href="/helpcenter">
+            <StyledIcon icon={MailIcon}></StyledIcon>
+            24x7 support
+          </LinkWithIcons>
+        </FooterBlock>
+        <FooterBlock>
+          <FooterBlockTitle>Download our apps</FooterBlockTitle>
+          <DownloadApps>
+            <DownloadIcons
+              href="https://itunes.apple.com/in/app/hopscotch-find-something-new/id945949424"
+              target="_blank"
+            >
+              <DownloadIcon icon={AppleIcon}></DownloadIcon>
+            </DownloadIcons>
+            <DownloadIcons
+              href="https://play.google.com/store/apps/details?id=in.hopscotch.android"
+              target="_blank"
+            >
+              <DownloadIcon icon={PlayIcon}></DownloadIcon>
+            </DownloadIcons>
+          </DownloadApps>
+        </FooterBlock>
+        <FooterBlock>
+          <FooterBlockTitle>Connect with us</FooterBlockTitle>
+        </FooterBlock>
+      </FooterSection>
+      <FooterLinks>
+        <FooterLink>
           <Link href="/about/AboutUs">About Us</Link>
-        </FooterElement>
-        <FooterElement>
+        </FooterLink>
+        <FooterLink>
           <Link href="/about/Terms">Terms of Service </Link>
-        </FooterElement>
-        <FooterElement>
+        </FooterLink>
+        <FooterLink>
           <Link href="/about/privacy">Privacy Policy</Link>
-        </FooterElement>
-      </FooterElements>
+        </FooterLink>
+      </FooterLinks>
       <Copyright>
         &#169; 2011-{currentYear} Hopscotch. All rights reserved.
       </Copyright>
--- packages/react-lib/ui-components/src/footer/FooterCss.tsx
+++ packages/react-lib/ui-components/src/footer/FooterCss.tsx
@@ -1,14 +1,30 @@
 import styled from '@emotion/styled';
-import { typography, HsTextAlign, Colors } from '@hs/utils';
+import { typography, HsTextAlign, Colors, secondaryColor } from '@hs/utils';
+import { SvgIcon } from '@hs/icons';
 
 const FooterWrapper = styled.footer`
   font-size: ${typography.size.s1}px;
   line-height: 13px;
   font-weight: ${typography.weight.medium};
   text-align: ${HsTextAlign.center};
+  background-color: ${Colors.WHITE};
 `;
 
-const FooterElements = styled.ul`
+const FooterSection = styled.section`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  > div {
+    flex: 50%;
+  }
+`;
+
+const FooterBlock = styled.div`
+  border-right: 1px solid ${Colors.GRAY50};
+  border-bottom: 1px solid ${Colors.GRAY50};
+`;
+
+const FooterLinks = styled.ul`
   margin: 0;
   list-style: none;
   display: flex;
@@ -16,11 +32,19 @@ const FooterElements = styled.ul`
   padding: ${typography.size.s3}px 0 4px 0;
 `;
 
-const FooterElement = styled.li``;
+const FooterLink = styled.li``;
 
 const Link = styled('a')`
   color: ${Colors.DARKGRAY};
   text-decoration: none;
+`;
+
+const LinkWithIcons = styled('a')`
+  color: ${Colors.GRAY20};
+  text-decoration: none;
+  display: inline-flex;
+  align-items: center;
+  height: 48px;
 `;
 
 const Copyright = styled.div`
@@ -30,4 +54,49 @@ const Copyright = styled.div`
   text-align: ${HsTextAlign.center};
 `;
 
-export { Copyright, FooterWrapper, FooterElements, FooterElement, Link };
+const StyledIcon = styled(SvgIcon)`
+  margin: 0 5px;
+  fill: white;
+  max-width: ${typography.size.s3}px;
+`;
+
+const FooterBlockTitle = styled.h5`
+  margin: 19px 0 0 0;
+  font-weight: ${typography.weight.medium};
+`;
+
+const DownloadApps = styled.div`
+  line-height: 16px;
+  margin: 10px 10px 15px;
+  color: ${secondaryColor[300]}px;
+`;
+
+const DownloadIcons = styled('a')`
+  border: 1px solid ${Colors.MERCURY};
+  border-radius: 14px;
+  padding: 0;
+  margin: 0 4px;
+  display: inline-block;
+  height: ${typography.size.m3}px;
+`;
+
+const DownloadIcon = styled(SvgIcon)`
+  margin: 0 5px;
+  fill: white;
+  max-width: ${typography.size.s3}px;
+`;
+export {
+  Copyright,
+  DownloadApps,
+  DownloadIcons,
+  DownloadIcon,
+  FooterWrapper,
+  FooterSection,
+  FooterBlock,
+  FooterBlockTitle,
+  FooterLinks,
+  FooterLink,
+  Link,
+  LinkWithIcons,
+  StyledIcon,
+};
