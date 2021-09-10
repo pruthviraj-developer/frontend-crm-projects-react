701ffc40778b4c3f94485e6554faafb10945a777
Pruthviraj Patel <pruthviraj@hopscotch.in>
2021-09-10 20:00:44 +0530

    Brand info displaying

--- packages/lib-react/components/src/accordion/Accordion.tsx
+++ packages/lib-react/components/src/accordion/Accordion.tsx
@@ -20,65 +20,83 @@ export const Accordian: FC<IAccordianProps> = ({
   productInfo,
 }: IAccordianProps) => {
   return (
-    <AccordianWrapper>
-      <AccordionTitle>Item details</AccordionTitle>
-      <AccordionContent>
-        <AccordianDescription>
-          {productInfo.productDesc && (
-            <DetailsDescription
-              dangerouslySetInnerHTML={{ __html: productInfo.productDesc }}
-            ></DetailsDescription>
-          )}
-          <DetailsDescription>
-            <OtherDetails>
-              <b>Suitable for</b>
-              <div className={'content'}>
-                {productInfo.simpleSkus[0] && productInfo.simpleSkus[0].gender}
-              </div>
-            </OtherDetails>
-            <OtherDetails>
-              <b>Colour</b>
-              <div className={'content'}>
-                {productInfo.simpleSkus[0] &&
-                  productInfo.simpleSkus[0].attributes.colour}
-              </div>
-            </OtherDetails>
-          </DetailsDescription>
-          {productInfo.productLevelAttrList &&
-            productInfo.productLevelAttrList.length &&
-            productInfo.productLevelAttrList.map((subAttr, index: number) => {
-              return (
-                subAttr.isShowAttr && (
-                  <DetailsDescription margin={true} key={index}>
-                    <b>{subAttr.attributeName}</b>
-                    {subAttr.attributeValue && (
-                      <DetailsDescriptionTitle>
-                        {subAttr.attributeValue}
-                      </DetailsDescriptionTitle>
-                    )}
-                    <FeatureAttributesList>
-                      {subAttr.productSubAttrList &&
-                        subAttr.productSubAttrList.length &&
-                        subAttr.productSubAttrList.map(
-                          (featureAttr, subIndex) => {
-                            return (
-                              featureAttr.attributeValue && (
-                                <FeatureAttributesListItem key={subIndex}>
-                                  {(featureAttr.isShowAttrName
-                                    ? `${featureAttr.subAttributeName} : `
-                                    : '') + featureAttr.attributeValue}
-                                </FeatureAttributesListItem>
-                              )
-                            );
-                          }
-                        )}
-                    </FeatureAttributesList>
-                  </DetailsDescription>
-                )
-              );
-            })}
-        </AccordianDescription>
-      </AccordionContent>
-    </AccordianWrapper>
+    <>
+      <AccordianWrapper>
+        <AccordionTitle>Item details</AccordionTitle>
+        <AccordionContent>
+          <AccordianDescription>
+            {productInfo.productDesc && (
+              <DetailsDescription
+                dangerouslySetInnerHTML={{ __html: productInfo.productDesc }}
+              ></DetailsDescription>
+            )}
+            <DetailsDescription>
+              <OtherDetails>
+                <b>Suitable for</b>
+                <div className={'content'}>
+                  {productInfo.simpleSkus[0] &&
+                    productInfo.simpleSkus[0].gender}
+                </div>
+              </OtherDetails>
+              <OtherDetails>
+                <b>Colour</b>
+                <div className={'content'}>
+                  {productInfo.simpleSkus[0] &&
+                    productInfo.simpleSkus[0].attributes.colour}
+                </div>
+              </OtherDetails>
+            </DetailsDescription>
+            {productInfo.productLevelAttrList &&
+              productInfo.productLevelAttrList.length &&
+              productInfo.productLevelAttrList.map((subAttr, index: number) => {
+                return (
+                  subAttr.isShowAttr && (
+                    <DetailsDescription margin={true} key={index}>
+                      <b>{subAttr.attributeName}</b>
+                      {subAttr.attributeValue && (
+                        <DetailsDescriptionTitle>
+                          {subAttr.attributeValue}
+                        </DetailsDescriptionTitle>
+                      )}
+                      <FeatureAttributesList>
+                        {subAttr.productSubAttrList &&
+                          subAttr.productSubAttrList.length &&
+                          subAttr.productSubAttrList.map(
+                            (featureAttr, subIndex) => {
+                              return (
+                                featureAttr.attributeValue && (
+                                  <FeatureAttributesListItem key={subIndex}>
+                                    {(featureAttr.isShowAttrName
+                                      ? `${featureAttr.subAttributeName} : `
+                                      : '') + featureAttr.attributeValue}
+                                  </FeatureAttributesListItem>
+                                )
+                              );
+                            }
+                          )}
+                      </FeatureAttributesList>
+                    </DetailsDescription>
+                  )
+                );
+              })}
+          </AccordianDescription>
+        </AccordionContent>
+      </AccordianWrapper>
+
+      {productInfo.showBrandDetails && productInfo.brandDescription && (
+        <AccordianWrapper>
+          <AccordionTitle>About {productInfo.brandName}</AccordionTitle>
+          <AccordionContent>
+            <AccordianDescription>
+              <DetailsDescription
+                dangerouslySetInnerHTML={{
+                  __html: productInfo.brandDescription,
+                }}
+              ></DetailsDescription>
+            </AccordianDescription>
+          </AccordionContent>
+        </AccordianWrapper>
+      )}
+    </>
   );
 };
