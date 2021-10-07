e76ce4c9e31d660e19ed0a181fb2112469167075
Rishikant Tiwari <rishikant.tiwari@hopscotch.in>
2021-10-07 18:55:07 +0530

    updated

--- packages/lib-react/framework/src/context/CartContext.tsx
+++ packages/lib-react/framework/src/context/CartContext.tsx
@@ -10,12 +10,12 @@ export const CartItemQtyContext = createContext<ICartContext>(
 export const CartItemQtyProvider: FC<unknown> = ({ children }) => {
   const [cartItemQty, setCartItemQuantity] = useState(0);
   const updateCartItemQty = (qty: number) => {
+    setCartItemQuantity(qty);
     const expireProp = {
       expires: new Date(
         timeService.getCurrentTime() + 30 * 24 * 60 * 60 * 1000
       ),
     };
-    setCartItemQuantity(qty);
     cookiesService.setCookies({
       key: COOKIE_DATA.CART_ITEM_QTY,
       value: qty,
