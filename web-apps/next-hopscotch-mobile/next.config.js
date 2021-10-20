865043f29afd3fe086e900dde3a3d3d7e14c6f74
Rishikant Tiwari <rishikant.tiwari@hopscotch.in>
2021-10-20 22:37:10 +0530

    Ignoring env setting

--- web-apps/next-hopscotch-mobile/next.config.js
+++ web-apps/next-hopscotch-mobile/next.config.js
@@ -5,11 +5,11 @@ module.exports = {
     domains: ['hopscotch.in'],
     imageSizes: [360, 480, 720, 1080],
   },
-  env: {
-    WEB_HOST: process.env.MY_ENV_VAR,
-    NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID: process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID,
-    ANALYTIC_HOST: process.env.ANALYTIC_HOST,
-  },
+  // env: {
+  //   WEB_HOST: process.env.MY_ENV_VAR,
+  //   NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID: process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID,
+  //   ANALYTIC_HOST: process.env.ANALYTIC_HOST,
+  // },
   async rewrites() {
     return [
       {
