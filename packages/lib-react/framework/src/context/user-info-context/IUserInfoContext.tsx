d4ca6e780f99436751ed42edaf25c401ee55a143
Pruthviraj Patel <pruthviraj@hopscotch.in>
2021-10-27 10:47:05 +0530

    user info api moved to user info context

--- packages/lib-react/framework/src/context/user-info-context/IUserInfoContext.tsx
+++ packages/lib-react/framework/src/context/user-info-context/IUserInfoContext.tsx
@@ -1,5 +1,4 @@
 import { IUserInfoProps } from 'product/types';
 export interface UserInfoProps {
   userInfo?: IUserInfoProps;
-  isLoading: boolean;
 }
--- packages/lib-react/framework/src/context/user-info-context/UserInfoContext.tsx
+++ packages/lib-react/framework/src/context/user-info-context/UserInfoContext.tsx
@@ -1,5 +1,12 @@
 import React, { createContext, FC } from 'react';
-import { userInfo } from '../../user-info';
+import { useQuery } from 'react-query';
+import {
+  productDetailsService,
+  timeService,
+  cookiesService,
+} from '@hs/services';
+import { IUserInfoProps } from 'product/types';
+import { COOKIE_DATA } from '../../storage';
 import { UserInfoProps } from './IUserInfoContext';
 
 export const UserInfoContext = createContext<UserInfoProps>(
@@ -7,8 +14,39 @@ export const UserInfoContext = createContext<UserInfoProps>(
 );
 
 export const UserInfoProvider: FC<unknown> = ({ children }) => {
+  const { data: userInfo, isSuccess: isSuccess } = useQuery<IUserInfoProps>(
+    'userInfo',
+    productDetailsService.getUserInfo,
+    {
+      staleTime: Infinity,
+      retry: false,
+    }
+  );
+  const expireProp = {
+    expires: new Date(timeService.getCurrentTime() + 30 * 24 * 60 * 60 * 1000),
+  };
+  if (isSuccess && userInfo) {
+    const setCookie = (key: string, value: any) => {
+      cookiesService.setCookies({
+        key,
+        value,
+        options: expireProp,
+      });
+    };
+    if (userInfo.isLoggedIn) {
+      setCookie(COOKIE_DATA.CUSTOMER_INFO, userInfo);
+      cookiesService.deleteCookie(COOKIE_DATA.GUEST_CUSTOMER_INFO);
+    } else {
+      setCookie(COOKIE_DATA.GUEST_CUSTOMER_INFO, userInfo);
+      cookiesService.deleteCookie(COOKIE_DATA.CUSTOMER_INFO);
+    }
+    if (userInfo.cartItemQty !== undefined) {
+      setCookie(COOKIE_DATA.CART_ITEM_QTY, userInfo.cartItemQty);
+    }
+    setCookie(COOKIE_DATA.PERSISTENT_TICKET, userInfo.persistentTicket);
+  }
   return (
-    <UserInfoContext.Provider value={{ ...userInfo() }}>
+    <UserInfoContext.Provider value={{ userInfo }}>
       {children}
     </UserInfoContext.Provider>
   );
