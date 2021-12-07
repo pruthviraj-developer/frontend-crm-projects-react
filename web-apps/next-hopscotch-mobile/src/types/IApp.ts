5a5e094e1533d93d5eb6a0b1c87382c29df64e95
Rishikant Tiwari <rishikant.tiwari@hopscotch.in>
2021-12-07 23:41:01 +0530

    Renamed to ignore from bootstrap

--- web-apps/next-hopscotch-mobile/src/types/IApp.ts
+++ web-apps/next-hopscotch-mobile/src/types/IApp.ts
@@ -1,4 +1,4 @@
-import { ReactElement, ReactNode } from '@/../../../packages/icons/node_modules/@types/react';
+import { ReactElement, ReactNode } from 'react';
 import { NextPage } from 'next';
 import { AppProps } from 'next/app';
 
