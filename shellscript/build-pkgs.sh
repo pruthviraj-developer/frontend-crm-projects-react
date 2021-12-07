fa2956feced5c82292b0844e4756ffed29998bc6
Rishikant Tiwari <rishikant.tiwari@hopscotch.in>
2021-12-07 22:05:32 +0530

    fixed compile error

--- shellscript/build-pkgs.sh
+++ shellscript/build-pkgs.sh
@@ -1,2 +1,3 @@
+lerna bootstrap
 lerna run --parallel  --scope \"@hs/services\" --scope \"@hs/icons\" --scope \"@hs/utils\"  --scope \"@hs/framework\" build
 lerna run   --scope \"@hs/components\" --scope \"@hs/containers\" build
