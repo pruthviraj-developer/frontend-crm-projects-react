29c426c0605a27e3e7de0a55cf4d574c0d915a9b
Rishikant Tiwari <rishikant.tiwari@hopscotch.in>
2021-12-08 01:34:36 +0530

    Removed unused code

--- shellscript/build-pkgs.sh
+++ shellscript/build-pkgs.sh
@@ -1,3 +1,3 @@
 lerna bootstrap --ignore @hs-crm*/*
-lerna run --parallel  --scope \"@hs/services\" --scope \"@hs/icons\" --scope \"@hs/utils\"  --scope \"@hs/framework\" build
-lerna run   --scope \"@hs/components\" --scope \"@hs/containers\" build
+lerna run --parallel  --scope \"@hs/services\" --scope \"@hs/icons\" --scope \"@hs/utils\" build
+lerna run   --scope \"@hs/components\" --scope \"@hs/containers\" --scope \"@hs/framework\" build
