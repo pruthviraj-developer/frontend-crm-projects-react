3c1e9d40c7331e533b51cea8e0268418e4443ac4
Rishikant Tiwari <rishikant.tiwari@hopscotch.in>
2022-02-08 00:58:15 +0530

    Fixed url when moving internally

--- web-apps/next-hopscotch/src/pages/product/[...urlParams]/index.tsx
+++ web-apps/next-hopscotch/src/pages/product/[...urlParams]/index.tsx
@@ -18,8 +18,9 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   const queryClient = new QueryClient();
   const productId = context.params?.urlParams?.[0] || '';
   const isMobile = context.req.headers['x-nv-device'] === 'sp';
-  const baseUrl = process.env.WEB_HOST;
-  const url = `${baseUrl}${context.resolvedUrl?.split('?')?.[0]}`;
+  const baseUrl = process.env.WEB_HOST as string;
+  let url = context.resolvedUrl?.split('?')?.[0];
+  url = url?.indexOf(baseUrl) > 0 ? url : `${baseUrl}${url}`;
   let error: IProductError | boolean = false;
   try {
     await queryClient.fetchQuery<IProductDetails>(
