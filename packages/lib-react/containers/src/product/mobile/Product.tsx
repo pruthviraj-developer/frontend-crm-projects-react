cc6d013c24cf1617e054d79c50efda18a5629a7c
Pruthviraj Patel <pruthviraj@hopscotch.in>
2022-02-21 17:18:11 +0530

    Showing promotions only if promo available

--- packages/lib-react/containers/src/product/mobile/Product.tsx
+++ packages/lib-react/containers/src/product/mobile/Product.tsx
@@ -114,7 +114,7 @@ const ProductMobile = ({
             deleteFromWishlist,
           }}
         />
-        {offerDetails && (
+        {offerDetails && offerDetails.isPromoAvailable && (
           <Offers
             {...{
               ...offerDetails,
