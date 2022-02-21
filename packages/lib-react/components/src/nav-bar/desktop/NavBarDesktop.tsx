f9fb7061a9cf9eee5c50baa9096c0c9e4ab5ce8b
Pruthviraj Patel <pruthviraj@hopscotch.in>
2022-02-21 16:56:53 +0530

    My accout redirecting properly

--- packages/lib-react/components/src/nav-bar/desktop/NavBarDesktop.tsx
+++ packages/lib-react/components/src/nav-bar/desktop/NavBarDesktop.tsx
@@ -89,8 +89,7 @@ export const NavBarDesktop: FC = () => {
         <NotificationBar>
           {showAccountNotification && <NotificationDot />}
           <NextNavLink
-            href="/my/account/orders"
-            queryParams={{ funnel: 'Account' }}
+            href="my/account/orders/?funnel=Account"
             name="Account"
             margin="0"
             padding="8px 12px 6px"
--- packages/lib-react/components/src/nav-bar/mobile/NavBar.tsx
+++ packages/lib-react/components/src/nav-bar/mobile/NavBar.tsx
@@ -64,7 +64,7 @@ export const NavBar: FC<INavBarProps> = ({ showSearchPopup }: INavBarProps) => {
         <NavLinkWrapper>
           {showAccountNotification && <NotificationDot />}
           <NextNavLink
-            href="/my/account/orders/"
+            href="my/account/orders/?funnel=Account"
             name="Account"
             display="inline-block"
             fontSize="1.2rem"
