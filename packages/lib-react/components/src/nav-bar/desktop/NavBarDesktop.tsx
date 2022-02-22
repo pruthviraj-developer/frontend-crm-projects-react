dec1feb5988b02e8020f0e8a5754ee2ed42dde79
Pruthviraj Patel <pruthviraj@hopscotch.in>
2022-02-22 12:47:58 +0530

    Link changes for url navigation

--- packages/lib-react/components/src/nav-bar/desktop/NavBarDesktop.tsx
+++ packages/lib-react/components/src/nav-bar/desktop/NavBarDesktop.tsx
@@ -121,7 +121,7 @@ export const NavBarDesktop: FC = () => {
             </Link>
             <FilterWrapper>
               <NextNavLink
-                href="/?funnel=Discover&amp;from_screen=product"
+                href="/"
                 name="Discover"
                 display="inline-block"
                 margin="0"
@@ -135,7 +135,7 @@ export const NavBarDesktop: FC = () => {
               {sortedTile && (
                 <FilteredBy>
                   <NextNavLink
-                    href="/?funnel=Discover&amp;from_screen=product&amp;ref=navigation&amp;department=null&amp;customTileId=null"
+                    href="/"
                     name={sortedTile}
                     display="inline-block"
                     margin="0"
@@ -145,7 +145,7 @@ export const NavBarDesktop: FC = () => {
               )}
             </FilterWrapper>
             <NextNavLink
-              href="/moments?funnel=Moments&amp;from_screen=product"
+              href="/moments"
               name="Moments"
               display="inline-block"
               margin="0 32px 0 0"
